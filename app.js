e020b437e69cf495fc736bd457a89dd5ea7ab4df
John Kenneth <johnkennethagpaoa@gmail.com>
2023-04-27 23:53:38 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+app.get("/", function (req, res) {
+    var today = new Date();
+    var currentDay = today.getDay();
+
+    if (currentDay === 6 || currentDay === 0) {
+        res.send("<h1>Yay it's the weekend!</h1>");
+    }
+    else {
+        res.sendFile(__dirname + "/index.html");
+    }
+});
+
+
+app.listen(3000, function () {
+    console.log("server running on port 3000");
+});
