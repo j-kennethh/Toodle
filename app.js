ab49f642aa65e7ac2ba94f80007eef46f7796dcd
John Kenneth <johnkennethagpaoa@gmail.com>
2023-05-06 23:10:05 +0800

    add lodash

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -27,21 +28,13 @@ const List = mongoose.model("List", listSchema);
 
 app.get("/", function (req, res) {
     Item.find({}).then((items) => {
-        if (items.length === 0) {
-            Item.insertMany(defaultItems).then((status) => {
-                console.log(status);
-            });
-            res.redirect("/");
-        }
-        else {
-            res.render("list", {listTitle: "Today", items: items});
-        }
+        res.render("list", {listTitle: "Today", items: items});
     });
 });
 
 
 app.get("/:listName", function (req, res) {
-    const listName = req.params.listName;
+    const listName = _.capitalize(req.params.listName);
 
     List.findOne({name: listName}).then((foundList) => {
         if (foundList) {
