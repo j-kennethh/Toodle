3c28cd374e374f4dce9f91ac073afe39c681d55f
John Kenneth <johnkennethagpaoa@gmail.com>
2023-05-06 20:35:11 +0800

    connect with mongoose

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -7,13 +8,43 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/toodleDB");
+const itemsSchema = new mongoose.Schema({
+    name: String
+});
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item ({
+    name: "Welcome to Toodle!"
+});
+
+const item2 = new Item ({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item ({
+    name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+
 
 
 app.get("/", function (req, res) {
     const day = date.getDate();
-    res.render("list", {listTitle: day, items: items});
+
+    Item.find({}).then((items) => {
+        if (items.length === 0) {
+            Item.insertMany(defaultItems).then((status) => {
+                console.log(status);
+            });
+            res.redirect("/");
+        }
+        else {
+            res.render("list", {listTitle: day, items: items});
+        }
+    });
 });
 
 app.post("/", function (req, res) {
